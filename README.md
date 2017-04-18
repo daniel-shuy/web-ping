76e875c2d649e3ee72a66a5146a9659dc2f8d184
Runjuu <mail@ollie.top>
2017-04-18 13:34:44 +0800

    模块化改造

--- README.md
+++ README.md
@@ -1,34 +1,12 @@
-# pingjs
-JavaScript ping API for use in a web browser context.  Released under the BSD-3-Clause license, see 
-LICENSE.  
+# web-ping
+JavaScript ping API for use in a web browser context.  Released under the BSD-3-Clause license, see
+LICENSE.
 
 ## Installation
 
-There are three possible ways to install this.  
-
-### HTML script tag
-
-Drag and drop the ping.js file next to an HTML file.  Inside the `<head>` tag 
-of the HTML file, use a script tag to load ping.js.  
-
-```html
-<script src="./ping.js"></script>
-```
-
-### NPM
-
-`npm install --save web-pingjs`
-
-### Bower
-
-`bower install --save web-pingjs`
+`npm install --save web-ping`
 
 ## Usage
-
-This library uses a  [UMD header](https://github.com/umdjs/umd/blob/master/templates/returnExports.js) 
-that allows it to be loaded as CommonJS, AMD, or a window global object.  
-
-A single function is exported, `ping`.  Ping's signature follows:  
 
 ```js
 /**
@@ -39,7 +17,7 @@ A single function is exported, `ping`.  Ping's signature follows:
  */
 ```
 
-Example:  
+Example:
 
 ```js
 ping('https://google.com/').then(function(delta) {
@@ -51,6 +29,6 @@ ping('https://google.com/').then(function(delta) {
 
 ## Caveats
 
-The user should be aware that this method relies on the HTTP protocol to ping 
-remote URLs.  Consequently, ping times are not as reliable as if they were 
-performed using the ICMP protocol.  
+The user should be aware that this method relies on the HTTP protocol to ping
+remote URLs.  Consequently, ping times are not as reliable as if they were
+performed using the ICMP protocol.
--- ping.js
+++ ping.js
@@ -1,46 +1,34 @@
 // See LICENSE for usage information
+// Thanks to https://github.com/jdfreder/pingjs
 
-// The following lines allow the ping function to be loaded via commonjs, AMD,
-// and script tags, directly into window globals.
-// Thanks to https://github.com/umdjs/umd/blob/master/templates/returnExports.js
-(function (root, factory) { if (typeof define === 'function' && define.amd) { define([], factory); } else if (typeof module === 'object' && module.exports) { module.exports = factory(); } else { root.ping = factory(); }
-}(this, function () {
+/**
+* Creates and loads an image element by url.
+* @param  {String} url
+* @param  {Number} timeout
+* @return {Promise} promise that resolves to an image element or
+*                   fails to an Error.
+*/
+const requestImage = (url, timeout) => (new Promise(function(resolve, reject) {
+  const img = new Image();
+  img.onload = resolve;
+  img.onerror = reject;
+  img.src = `${url}?random-no-cache=${Math.floor((1 + Math.random()) * 0x10000).toString(16)}`;
 
-    /**
-     * Creates and loads an image element by url.
-     * @param  {String} url
-     * @return {Promise} promise that resolves to an image element or
-     *                   fails to an Error.
-     */
-    function request_image(url) {
-        return new Promise(function(resolve, reject) {
-            var img = new Image();
-            img.onload = function() { resolve(img); };
-            img.onerror = function() { reject(url); };
-            img.src = url + '?random-no-cache=' + Math.floor((1 + Math.random()) * 0x10000).toString(16);
-        });
-    }
-
-    /**
-     * Pings a url.
-     * @param  {String} url
-     * @param  {Number} multiplier - optional, factor to adjust the ping by.  0.3 works well for HTTP servers.
-     * @return {Promise} promise that resolves to a ping (ms, float).
-     */
-    function ping(url, multiplier) {
-        return new Promise(function(resolve, reject) {
-            var start = (new Date()).getTime();
-            var response = function() { 
-                var delta = ((new Date()).getTime() - start);
-                delta *= (multiplier || 1);
-                resolve(delta); 
-            };
-            request_image(url).then(response).catch(response);
-            
-            // Set a timeout for max-pings, 5s.
-            setTimeout(function() { reject(Error('Timeout')); }, 5000);
-        });
-    }
-    
-    return ping;
+  clearTimeout(requestImage.timeout);
+  if (timeout) requestImage.timeout = setTimeout(reject, timeout);
 }));
+
+/**
+ * Pings a url.
+ * @param  {String} url
+ * @param  {Number} timeout
+ * @return {Promise} promise that resolves to a ping (ms, float).
+ */
+async function ping(url, timeout) {
+  const start = (new Date()).getTime();
+
+  await requestImage(url, timeout);
+  return (new Date()).getTime() - start;
+}
+
+export default ping;
